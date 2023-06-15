33c477018c87e62b9eca4b19eb1a22f943ef6647
FijiCookie <91664070+FijiCookie@users.noreply.github.com>
2023-06-15 20:04:51 +0200

    Commit again

---
+++ daten.py
@@ -0,0 +1,33 @@
+import json
+
+
+# Diese Funktion speicher die eingegebene Daten in einem Jason-File (Json-File wird geschrieben "w")
+def speichern(name, kategorie, mhd, bewertung):
+    datei = "ausgabe_dictonary.json"
+    try:
+        with open(datei) as open_file:
+            datei_inhalt = json.load(open_file)
+    except FileNotFoundError:
+        datei_inhalt = {}
+
+    datei_inhalt[name] = {'Name': name,
+                          'Kategorie' : kategorie,
+                          'MHD': mhd,
+                          'Bewertung': bewertung}
+
+    with open(datei, "w") as open_file:
+        json.dump(datei_inhalt, open_file, indent=4)
+
+
+# Funktion, um gespeicherte Daten (vom Json-File) wieder laden zu können
+def eingabe_laden():
+    datei_name = "ausgabe_dictonary.json"
+
+    try:
+        with open(datei_name) as open_file:
+            datei_inhalt = json.load(open_file)
+    except FileNotFoundError:
+        datei_inhalt = {}
+
+    return datei_inhalt
+
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from flask import render_template
 from flask import request
 from flask import redirect
 import json
+import daten
+from datetime import datetime
 
 
 #test
@@ -13,62 +15,106 @@ import json
 app = Flask("My_Fridge")
 
 
-daten_lebensmittel = 'daten.json'
+# daten_lebensmittel = 'daten.json'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         name = request.form['name']
+        kategorie = request.form['kategorie']
         mhd = request.form['mhd']
         bewertung = int(request.form['bewertung'])
-        lebensmittel = {'name': name, 'mhd': mhd, 'bewertung': bewertung}
+        daten.speichern(name, kategorie, mhd, bewertung)
 
-        try:
-            with open(daten_lebensmittel, 'r') as file:
-                daten = json.load(file)
-        except FileNotFoundError:
-            daten = {}
+        eintrag_gespeichert = "Du hast ein Lebensmittel in den Kühlschrank hinzugefügt. Willst du mehr hinzufügen?."
 
-        daten[name] = lebensmittel
+        return render_template('index.html', eintrag=eintrag_gespeichert)
 
-        with open(daten_lebensmittel, 'w') as file:
-            json.dump(daten, file)
-
-        return render_template('index.html')
     return render_template('index.html')
 
-@app.route('/loeschen/<name>')
-def loeschen(name):
-    try:
-        with open(daten_lebensmittel, 'r') as file:
-            daten = json.load(file)
-    except FileNotFoundError:
-        daten = {}
 
-    if name in daten:
-        del daten[name]
+# Hat fast funktioniert!!
+# @app.route('/statistik', methods=['GET', 'POST'])
+# def ausgabe():
+#     eingabe = daten.eingabe_laden()  # holt Daten aus daten.py
+#     filter_liste = []
+#     filter_value = ""
+#     filter_key = ""
+#     gefiltert = False
+#
+#     if request.method == 'POST':
+#         gefiltert = True
+#         mhd = request.form['mhd']
+#
+#         if mhd != "":
+#             filter_value = mhd
+#             filter_key = "MHD"
+#
+#         for key, eintrag in eingabe.items():
+#             if eintrag[filter_key] == filter_value:
+#                 filter_liste.append(eintrag)
+#
+#     # Daten sortieren nach dem aktuellen Datum
+#     eingabe_sortiert = dict(sorted(eingabe.items(), key=lambda x: datetime.strptime(x[1]['MHD'], '%d-%m-%y')))
+#
+#     return render_template('statistik.html', eintraege=eingabe_sortiert, gefilterte_eintraege=filter_liste, ist_gefiltert=gefiltert)
+def eingabe_laden():
+    with open('ausgabe_dictonary.json', 'r') as file:
+        daten = json.load(file)
+    return daten
 
-        with open(daten_lebensmittel, 'w') as file:
-            json.dump(daten, file)
+@app.route('/statistik', methods=['GET', 'POST'])
+def ausgabe():
+    eingabe = eingabe_laden()  # holt Daten aus JSON-Datei
 
-    return redirect('/')
+    # Daten sortieren nach dem aktuellen Datum
+    eingabe_sortiert = sorted(eingabe.items(), key=lambda x: datetime.strptime(x[1]['MHD'], '%d-%m-%y'))
 
-@app.route('/visualisierung')
-def visualisierung():
-    try:
-        with open(daten_lebensmittel, 'r') as file:
-            daten = json.load(file)
-    except FileNotFoundError:
-        daten = {}
+    return render_template('statistik.html', eintraege=eingabe_sortiert)
 
-    df = pd.DataFrame.from_dict(daten, orient='index', columns=['name', 'mhd', 'bewertung'])
+# @app.route('/loeschen/<name>')
+# def loeschen(name):
+#     try:
+#         with open(daten_lebensmittel, 'r') as file:
+#             daten = json.load(file)
+#     except FileNotFoundError:
+#         daten = {}
+#
+#     if name in daten:
+#         del daten[name]
+#
+#         with open(daten_lebensmittel, 'w') as file:
+#             json.dump(daten, file)
+#
+#     return redirect('/')
 
-    fig = px.bar(df, x='name', y='bewertung')
+# Fehlerseiten bei ungültigem URl (Seite nicht gefunden)
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
 
-    fig.write_html('statistik.html')
 
-    return render_template('statistik.html')
+# Interner Serverfehler URL
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
+# @app.route('/visualisierung')
+# def visualisierung():
+#     try:
+#         with open(daten_lebensmittel, 'r') as file:
+#             daten = json.load(file)
+#     except FileNotFoundError:
+#         daten = {}
+#
+#     df = pd.DataFrame.from_dict(daten, orient='index', columns=['name', 'mhd', 'bewertung'])
+#
+#     fig = px.bar(df, x='name', y='bewertung')
+#
+#     fig.write_html('statistik.html')
+#
+#     return render_template('statistik.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
